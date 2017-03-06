3329ae582b13978d8d3f2519f2a7ea50aa33fb88
pmwhite <pmw1197@gmail.com>
2017-03-06 16:57:59 -0500

    Update Packet.java

--- lightsysnetwork/Packet.java
+++ lightsysnetwork/Packet.java
@@ -4,8 +4,17 @@
  * and open the template in the editor.
  */
 package lightsysnetwork;
-import java.util.*;
-import java.time.format.DateTimeFormatter;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 import org.apache.commons.math3.ml.clustering.Clusterable;
 
 /**
@@ -25,28 +34,56 @@ public class Packet implements Clusterable {
         name = n;
     }
     
-    public static Packet parsePacket(String input) {
+    public String toString(){
+        return timestamp + "," + length + "," + in + "," + name;
+    }
+    
+    public static Packet parsePacket(String input) throws ParseException {
         Scanner scanner = new Scanner(input);
+        
+        SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS");
+        
+        String time = scanner.next() + " ";
+        scanner.next();
         scanner.useDelimiter(",");
+        time += scanner.next();
         
-        String date = scanner.next();
-        int length = Integer.parseInt(scanner.next());
-        boolean isIn = scanner.next() == "in";
-        String name = scanner.next();
+        Date date = f.parse(time);
+        long timestamp = date.getTime();
         
-        
-        DateTimeFormatter f = new DateTimeFormat.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
-        
-       return null;
-        
-        
-        
+        int len = Integer.parseInt(scanner.next());
+        boolean isIn = scanner.next().equals("in");
+        String n = scanner.next();
+        return new Packet(timestamp, len, isIn, n);
+    }
+    
+    public static ArrayList<Packet> parseFile(File file) throws FileNotFoundException, ParseException {
+        Scanner scanner = new Scanner(file);
+        ArrayList<Packet> result = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Packet packet = parsePacket(line);
+            result.add(packet);
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) throws IOException, ParseException {
+        System.out.println("Parsing...");
+        ArrayList<Packet> p = parseFile(new File("traffic_2017_03_06_a.csv"));
+        System.out.println("Outputting...");
+        File f = new File("output.out");
+        FileOutputStream d = new FileOutputStream(f);
+        for(Packet a : p){
+            d.write(a.toString().getBytes());
+        }
     }
 
     @Override
     public double[] getPoint() {
+        double dirValue = in ? 1 : 0;
         double[] result = new double[] {
-            timestamp, length, in
+            timestamp, length, dirValue
         };
         return result;
     }
