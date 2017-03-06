c656be0d7b3ac6463d6050c3e8ba70a26763dda1
Timothy Smith <timothyglensmith@cedarville.edu>
2017-03-06 15:25:19 -0700

    LightSysNetwork

--- lightsysnetwork/LightSysNetwork.java
+++ lightsysnetwork/LightSysNetwork.java
@@ -1,5 +1,10 @@
 package lightsysnetwork;
+import java.io.*;
+import java.text.*;
+import java.util.*;
 
+import lightsysnetwork.Packet;
+import lightsysnetwork.AnimalCorrelator;
 /**
  *
  * @author stall
@@ -9,8 +14,9 @@ public class LightSysNetwork {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, ParseException {
+	ArrayList<Packet> packets = Packet.parseFile(new File("test.csv"));
+    	double[][] res = AnimalCorrelator.Correlator(packets);	
     }
     
 }
