01d7764a548330b6cdee67cd8be8f5a51c367a74
pmwhite <pmw1197@gmail.com>
2017-03-06 17:36:46 -0500

    Update LightSysNetwork.java

--- lightsysnetwork/LightSysNetwork.java
+++ lightsysnetwork/LightSysNetwork.java
@@ -3,8 +3,6 @@ import java.io.*;
 import java.text.*;
 import java.util.*;
 
-import lightsysnetwork.Packet;
-import lightsysnetwork.AnimalCorrelator;
 /**
  *
  * @author stall
@@ -15,8 +13,8 @@ public class LightSysNetwork {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, ParseException {
-	ArrayList<Packet> packets = Packet.parseFile(new File("test.csv"));
-    	double[][] res = AnimalCorrelator.Correlator(packets);	
+	ArrayList<Packet> packets = Packet.parseFile(new File("traffic_2017_03_06_a.csv"));
+    	double[][] res = AnimalCorrelator.Correlator(packets);
     }
     
 }
